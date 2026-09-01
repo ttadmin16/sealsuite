28d07a153a22f7bbda8ec2a847adc9b1dff4add4
ttadmin16 <ucantsubpoename@atomicmail.io>
2026-09-02 00:35:40 +0100

    Update localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,1 +1,9 @@
+const bytes = new Uint8Array(96);
+crypto.getRandomValues(bytes);
 
+const msTokenValue = btoa(String.fromCharCode(...bytes))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+
+localStorage.setItem("msToken", msTokenValue);
